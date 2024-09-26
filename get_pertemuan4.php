4d6e81aa11c4cefab4473e906a8c9675c6e5d46e
Modaminstitute <tjiamnickelmodami@gmail.com>
2024-09-26 16:05:32 +0700

    Add files via upload

---
+++ get_pertemuan4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="GET" action="SimpleClass_pertemuan4.php">
+        <table>
+            <tr>
+                <td><input type="text" name="email" placeholder="Masukan email" /></td>
+            </tr>
+            <tr>
+                <td><input type="password" name="password" placeholder="Masukan password" /></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="Kirim" /></td>
+            </tr>
+        </table>
+
+    </form>
+</body>
+
+</html>
