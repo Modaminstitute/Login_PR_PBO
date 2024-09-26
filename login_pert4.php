0de10fabff222d3f36001fd397b46640ba9fd91f
Modami <tjiamnickelmodami@gmail.com>
2024-09-26 23:05:58 +0700

    Add files via upload

---
+++ login_pert4.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stylish Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="form-container">
+        <h2>Login Form</h2>
+        <form method="GET" action="SimpleClass_pertemuan4.php">
+            <table>
+                <tr>
+                    <td><input type="text" name="email" placeholder="Enter your email" required /></td>
+                </tr>
+                <tr>
+                    <td><input type="password" name="password" placeholder="Enter your password" required /></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Submit" /></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+
+    <footer>
+        <p class="small-text">by Nickel M and Nicholas ST</p>
+    </footer>
+</body>
+
+</html>
